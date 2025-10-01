25ef17bcda078bbd042b1db55112c4534746843b
Pedro Monteiro <pmapm@ua.pt>
2025-10-01 17:48:58 +0100

    initial

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -2,23 +2,36 @@
 import React from "react";
 import "./Team.css";
 import user from './user.png';
+import anibal from './Anibal .jpg';
+import daniel from './Daniel.jpg';
+import eunice from './Eunice.jpg';
+import fabio from './Fabio (1).jpg';
+import joaquim from './Joaquim Monteiro.jpg';
+import lucinda from './Lucinda.jpg';
+import marina from './Marina.jpg';
+import patricia from './Patricia Seixo.jpg';
+import paulo from './Paulo Saraiva.jpg';
+import rui from './Rui.jpg';
+import telmo from './Telmo Silva.jpg';
+import tiago from './Tiago Robalo.jpg';
+import tania from './tania.jpg';
 
 // ⚠️ Coloca as tuas imagens em /public/imagens/equipa/…
 // ou importa-as (ex.: import joao from './joao.jpg')
 const members = [
-  { name: "Joaquim Monteiro", role: "Reformado", photo: user },
-  { name: "Paulo Saraiva", role: "Eng. Eletrotécnico",photo: user },
-  { name: "Patrícia Seixo", role: "Animadora Socio-cultural", photo: user },
-  { name: "Eunice dos Santos", role: "Assessora de Direção", photo: user },
-  { name: "Tiago Robalo", role: "Serralheiro", photo: user },
-  { name: "Fábio Martins", role: "Gestor", photo: user },
-  { name: "Tânia Carreira", role: "Educadora Social",photo: user },
-  { name: "Telmo da Silva", role: "Empresário", photo: user },
-  { name: "Daniel Costa", role: "", photo: user },
-  { name: "Marina Marujo", role: "Empregada de Balcão", photo: user },
-  { name: "Anibal Soares", role: "Operário de captação de água subterrânea", photo: user },
-  { name: "Rui dos Santos", role: "", photo: user },
-  { name: "Lucinda Tomé", role: "", photo: user },
+  { name: "Joaquim Monteiro", role: "Reformado", photo: joaquim },
+  { name: "Paulo Saraiva", role: "Eng. Eletrotécnico",photo: paulo },
+  { name: "Patrícia Seixo", role: "Animadora Socio-cultural", photo: patricia },
+  { name: "Eunice dos Santos", role: "Assessora de Direção", photo: eunice },
+  { name: "Tiago Robalo", role: "Serralheiro", photo: tiago },
+  { name: "Fábio Martins", role: "Gestor", photo: fabio },
+  { name: "Tânia Carreira", role: "Educadora Social",photo: tania },
+  { name: "Telmo da Silva", role: "Empresário", photo: telmo },
+  { name: "Daniel Costa", role: "", photo: daniel },
+  { name: "Marina Marujo", role: "Empregada de Balcão", photo: marina },
+  { name: "Anibal Soares", role: "Operário de captação de água subterrânea", photo: anibal },
+  { name: "Rui dos Santos", role: "", photo: rui },
+  { name: "Lucinda Tomé", role: "", photo: lucinda },
 ];
 
 export default function Team() {
