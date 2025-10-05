860639a8a71588dbfe2caf7a242540d09c557b7d
Pedro Monteiro <pmapm@ua.pt>
2025-10-05 16:51:21 +0100

    change modal

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
     <div>
       <AnnouncementModal />
       <Hero />
-      <Livestream channelName="vilafernandocomambicao" />
       <Program />
       <Team />
       <ContactForm />
